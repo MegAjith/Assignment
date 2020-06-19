ff878c9a2a159e9cd30e5555e9ef8032f46fb24f
MegAjith <64888099+MegAjith@users.noreply.github.com>
2020-06-19 21:37:55 +0530

    Add files via upload

---
+++ easier/SpeakHello.js
@@ -0,0 +1,18 @@
+// STEP 2: Wrap the entire contents of SpeakHello.js inside of an IIFE
+// See Lecture 52, part 2
+
+
+// STEP 3: Create an object, called 'helloSpeaker' to which you will attach
+// the "speak" method and which you will expose to the global context
+// See Lecture 52, part 1
+
+(function (window) {
+  var helloSpeaker = {};
+  var speakword = "Hello ";
+  helloSpeaker.speak = function (name) {
+    console.log(speakword+" "+name);
+  }
+
+  window.helloSpeaker = helloSpeaker;
+
+})(window);
